a42a34304735abd5f0a4768ff0f1d5a2053c1c69
Zoe <zoeervin2@gmail.com>
2024-12-21 23:35:30 -0600

    creating source dir for files and moving them

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";//this is the helper function for joint class names
+import { cn } from "@/src/app/lib/utils";//this is the helper function for joint class names
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
