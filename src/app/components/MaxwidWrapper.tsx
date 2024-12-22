46209cf71aa60be6b2c6b5ae848d31c77e676255
Zoe <zoeervin2@gmail.com>
2024-12-21 23:53:18 -0600

    cn funct is actually this one the other one was using the maxwidwrapper

--- src/app/components/MaxwidWrapper.tsx
+++ src/app/components/MaxwidWrapper.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from "react";
+import { cn } from "../lib/utils";
 //creating maxwid function
 const MaxwidWrapper = ({className, children}:{ className?: string                 //putting this on new line so its read seprately 
                                                children:ReactNode}) => {
-                                            return(<div className='mx-auto w-full max-w-screen-xl'></div>)        }//max-w-screen-xl is a 1280 pxl screen limit
+                                            return(<div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>{children}</div>)        }//max-w-screen-xl is a 1280 pxl screen limit
+export default MaxwidWrapper//allowing use of this component in other files
