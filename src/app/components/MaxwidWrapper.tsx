83cec6a23ff20b13db468042deead75cbdc375ec
Zoe <zoeervin2@gmail.com>
2024-12-21 23:55:03 -0600

    adding comments

--- src/app/components/MaxwidWrapper.tsx
+++ src/app/components/MaxwidWrapper.tsx
@@ -3,5 +3,6 @@ import { cn } from "../lib/utils";
 //creating maxwid function
 const MaxwidWrapper = ({className, children}:{ className?: string                 //putting this on new line so its read seprately 
                                                children:ReactNode}) => {
-                                            return(<div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>{children}</div>)        }//max-w-screen-xl is a 1280 pxl screen limit
+                                            return(<div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className//if i pass this in itll override the preset className
+                                            )}>{children}</div>)        }//max-w-screen-xl is a 1280 pxl screen limit
 export default MaxwidWrapper//allowing use of this component in other files
