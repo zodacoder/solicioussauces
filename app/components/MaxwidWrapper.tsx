2184361735d64d7f94420ea2e539b3185c6c6294
Zoe <zoeervin2@gmail.com>
2024-12-22 00:20:30 -0600

    fixing maxwid import

--- app/components/MaxwidWrapper.tsx
+++ app/components/MaxwidWrapper.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
-import { cn } from "../../../src/app/lib/utils";
+import { cn } from "../lib/utils";
+
 //creating maxwid function
 const MaxwidWrapper = ({className, children}:{ className?: string                 //putting this on new line so its read seprately 
                                                children:ReactNode}) => {
