ffd8f74fddf0b2962efb43deec920bed99eb233d
Zoe <zoeervin2@gmail.com>
2024-12-23 12:51:33 -0600

    adding div elements that display the perks and perk type descript

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import MaxwidWrapper from "./components/MaxwidWrapper";
 import Link from "next/link";
 import { Button, buttonVariants } from "./components/button";
-import { BadgeCheck, Check } from "lucide-react";
+import { BadgeCheck} from "lucide-react";
 
 
 const Pperks = [
@@ -20,9 +20,12 @@ const Pperks = [
       name: "Free Samples and Tasting Kits ",
       Icon: BadgeCheck,
       description: "Subcribe to our emails and recieve a free sample kit with our 3 flavors, only pay for shipping."
-  
-
-  }
+  },
+  {
+    name: "Food Saftey Approved",
+    Icon: BadgeCheck,
+    description: "Every featured product has been approved by the FDA"
+}
 ]
 
 
@@ -51,7 +54,21 @@ export default function Home() {
 
   <MaxwidWrapper className='py-20'>
     <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'>
-      
+      {Pperks.map((perk) => (
+          <div key={perk.name} className='text-center md:flex md:items-start md:text-left lg:block lg:text-center'>
+           <div className='md:flex-shrink-0 flex justify-center'>
+            <div className='h-16 w-16 flex items-center justify-center rounded-full bg-violet-100 text-violet-900'>
+              {<perk.Icon className='w-1/3 h-1/3'/>}
+            </div>
+            </div>
+            <div className='mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6'></div>
+             <h3 className='text-base font-medium text-gray-900'>{perk.name}</h3>
+             <p className='mt-3 text-sm text-muted-foreground'>
+
+              {perk.description}
+             </p>
+          </div>
+      ))}{/*using round braces instead of curly to directly return jsx*/}
       </div>{/* this is unique features section what makes solicious different from competition*/ }
     </MaxwidWrapper> 
 
