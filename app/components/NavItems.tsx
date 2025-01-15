8b996ef3052230b7ac7317e7b4ddcb05dda52c9e
Zoe <zoeervin2@gmail.com>
2025-01-15 15:03:48 -0600

    adding client side function and elements

--- app/components/NavItems.tsx
+++ app/components/NavItems.tsx
@@ -1,9 +1,39 @@
 "use client"
 
+import { useState } from 'react'
+import { PRODUCT_CATEGORIES } from '../config'
+import NavItem from './NavItem'
+
 const NavItems = () => {
 
+const [activeIndex, setActiveIndex] = useState< null | number > (null)
 
-return ()
+
+
+
+
+
+return <div className='flex gap-4 h-full'>
+
+{PRODUCT_CATEGORIES.map((category, index) =>{
+
+const handleOpen = () => {
+
+    if(activeIndex === index) {
+        setActiveIndex(null)
+    } else{
+        setActiveIndex(index)
+    }
+} 
+
+const isOpen = index === activeIndex
+
+return (<NavItem/>)
+
+
+})}
+
+</div>
 
 
 
